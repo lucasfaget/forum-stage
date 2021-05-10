da896b93e19d75ce38198927cb6cef08155e69e4
Valentin Laporte <valentin.laporte@hotmail.fr>
2021-05-10 21:23:28 +0200

    Commit VL : 10.05.21

--- config.php
+++ config.php
@@ -1,34 +1,15 @@
 <?php
 
-	/* ----A UTILISER SUR LE SITE d'HERGEMENT -------
+	/* ----A UTILISER SUR LE SITE d'HEBERGEMENT -------
 	$server = 'fdb28.awardspace.net';
 	$db     = '3788709_forumstage';
 	$login  = '3788709_forumstage';
 	$mdp    = '4frHtZ+S46M:WArK';
-	
-	try{
-
-		$link = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
-
-	}catch( Exception$e){
-
-		die('Erreur : ' . $e->getMessage());
-	}
 	--------------------------------------------------*/
-
 
 	/* ----A UTILISER EN LOCALHOST ------------------ */
 	$server = 'localhost';
 	$login  = 'root';
 	$mdp    = '';
 	$db     = 'forum_stage';
-
-		try {
-
-			$link = new PDO("mysql:host=$server;dbname=$db", $login, $mdp);
-
-		} catch(Exception $e){
-
-			die('erreur='.$e->getMessage());
-		}
 ?>
