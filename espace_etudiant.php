b76b1a079fd76e8ad924a86d3dc752080a6f47e1
Valentin Laporte <valentin.laporte@hotmail.fr>
2021-05-10 23:09:56 +0200

    Commit VL : 10.05.21

--- espace_etudiant.php
+++ espace_etudiant.php
@@ -1,58 +1,189 @@
 <?php
-        session_start();
-	require 'util.php';
-        require 'config.php';
+    session_start();
+	require ("util.php");
+    require ("connexion.php");
 	
-	//bouton déconnexion
+	//Bouton déconnexion <===== A METTRE DANS LE HEADER ?
 	if(isset($_GET['action']) && !empty($_GET['action']) && $_GET['action'] == 'logout'){
 		detruireSession();
 		header('Location: index.php');
 	}
+
+	//Si un bouton de désinscription est appuyé, on supprime le créneau de la base
+	if(isset($_POST['desinscrireStage'])){
+
+		$bdd = connexionservermysql($server, $db, $login, $mdp);
+		$reqDesinscrireCreneau = $bdd->prepare('DELETE 	FROM 
+															reserver
+														WHERE
+															reserver.Id_stage = ?
+														AND 
+															reserver.Id_etudiant = ?');
+		$reqDesinscrireCreneau->execute(array($_POST['idStage'], $_POST['idEtudiant']));
+	}
+
+	//Si le bouton de modification d'informations est appuyé, on met à jour la BDD
+	if(isset($_POST['modifEtudiant'])){
+
+		$bdd = connexionservermysql($server, $db, $login, $mdp);
+		$reqModifEtudiant = $bdd->prepare('	UPDATE etudiant
+											SET
+												etudiant.Mail = ?,
+												etudiant.NomEtu = ?,
+												etudiant.PrenomEtu = ?,
+												etudiant.Diplome = ?
+											WHERE
+												etudiant.Id_etudiant = ?');
+		$reqModifEtudiant->execute(array(	$_POST['mailEtudiant'], 
+											$_POST['nomEtudiant'], 
+											$_POST['prenomEtudiant'], 
+											$_POST['promoEtudiant'],
+											$_POST['idEtudiant']));
+	}
 ?>
+
 <!DOCTYPE HTML>
 <html lang="fr">
-	<head>
-		<meta charset="utf-8">
-		<title>Accueil | Forum Stage</title>
-	</head>
-	<body>
-		<!--L'utilisateur doit être connecté pour voir cette page-->
-		<?php if(estConnecte() && $_SESSION['userType'] == 'étudiant'): ?>
-                
-                <h1>Espace Etudiant</h1>
+<head>
+	<meta charset="utf-8">
+	<title>Accueil | Forum Stage</title>
+</head>
+<body>
+	<header>
+			
 
-                <!--Message temporaire pour la démo-->
-		<p> Bienvenue, vous vous êtes connecté avec <?= htmlspecialchars($_SESSION['userId']) ?>.
-		Votre type de compte est <?= htmlspecialchars($_SESSION['userType']) ?>.</p>
+	</header>
+<?php 
+	if(estConnecte() && $_SESSION['userType'] == 'étudiant'){ 
+?>
+	<main>   
 
-		<!--Boutons de navigation temporaires (ils seront dans le header-->
-		<nav>
-			<ul>
-				<li><a href="accueil_connecte.php">Planning</a></li>
-				<li><a href="chercher_stage.php">Chercher un stage</a></li>
-				<li><a href="liste_entreprises.php">Entreprises présentes</a></li>
-				<?php if($_SESSION['userType'] == 'entreprise'):?>
-				<li><a href="espace_entreprise.php">Votre espace</a></li>
-				<?php elseif($_SESSION['userType'] == 'étudiant'):?>
-				<li><a href="espace_etudiant.php">Votre espace</a></li>
-				<?php elseif($_SESSION['userType'] == 'administrateur'):?>
-				<li><a href="espace_admin.php">Votre espace</a></li>
-                                <?php endif;?>
-                                <li><a href="accueil_connecte.php?action=logout">Se déconnecter</a></li>
-			</ul>
-		</nav>
-                
-                <hr>
-		<h3>Vos informations</h3>
-		<p>Contenu à venir....</p>
-		
-		<hr>
-		<h3>Créneaux resvervé</h3>
-		<p>Contenu à venir....</p>
-                
-                <?php
-                else:
-                       echo "Merci de vous <a href='index.php'>connecter</a> pour voir cette page.";
-                endif; ?>
-	</body>
+<?php
+	$bdd = connexionservermysql($server, $db, $login, $mdp);
+	$reqEspaceEtudiant = $bdd->prepare('SELECT * 	FROM 
+														etudiant 
+													WHERE 
+														Mail = ?');
+	$reqEspaceEtudiant->execute(array($_SESSION['userId']));
+	$dataInfos = $reqEspaceEtudiant->fetch();
+?>
+
+    <h1>Votre Espace Etudiant</h1>
+
+	<h3>Vos informations</h3>
+	<form action="./espace_etudiant.php" method="POST">
+		<input 	type="hidden" 
+				name="idEtudiant"
+				value="<?php echo $dataInfos['Id_etudiant']; ?>">
+
+		<p>Nom :</p>
+		<input 	type="text" 
+				name="nomEtudiant"
+				value="<?php echo $dataInfos['NomEtu']; ?>">
+
+		<p>Prénom :</p>
+		<input 	type="text" 
+				name="prenomEtudiant"
+				value="<?php echo $dataInfos['PrenomEtu']; ?>">
+
+		<p>Adresse mail :</p>
+		<input 	type="text" 
+				name="mailEtudiant"
+				value="<?php echo $dataInfos['Mail']; ?>">
+
+		<p>Promotion :</p>
+		<select name="promoEtudiant">
+			<option selected value="<?php echo $dataInfos['Diplome']; ?>">
+				<?php echo $dataInfos['Diplome']; ?>
+			</option>
+<?php
+	if($dataInfos['Diplome'] != "DUT Informatique - 1ère année"){
+?>
+			<option>DUT Informatique - 1ère annee</option>
+<?php
+	}
+	if($dataInfos['Diplome'] != "DUT Informatique - 2ème année"){
+?>
+			<option>DUT Informatique - 2ème année</option>
+<?php
+	}
+	if($dataInfos['Diplome'] != "DUT Informatique - Année spéciale"){
+?>
+			<option>DUT Informatique - Année spéciale</option>
+<?php
+	}
+	if($dataInfos['Diplome'] != "Licence Pro - DQL"){
+?>
+			<option>Licence Pro - DQL</option>
+<?php
+	}
+	if($dataInfos['Diplome'] != "Licence Pro - GTIDM"){
+?>
+			<option>Licence Pro - GTIDM</option>
+<?php
+	}
+?>
+		</select>
+		<button type="submit"
+				name="modifEtudiant">
+			Enregistrer les modifications
+		</button>
+	</form>
+
+	<!------------------------------------------------------------------------------------------------>
+<?php
+	$reqListeStageEtu = $bdd->prepare('SELECT *	FROM 
+													stage, reserver, etudiant, entreprise 
+												WHERE
+													entreprise.Id_entreprise = stage.Id_entreprise
+												AND
+													stage.Id_stage = reserver.Id_stage
+												AND
+													reserver.Id_etudiant = etudiant.Id_etudiant
+												AND
+													etudiant.Id_etudiant = ?');
+	$reqListeStageEtu->execute(array($dataInfos['Id_etudiant']));
+?>
+	<p>Créneaux réservés :</p>
+
+	<table>
+		<tr>
+			<td>Intitulé du stage</td>
+			<td>Organisme</td>
+			<td>Heures</td>
+			<td>Se désinscrire</td>
+		</tr>
+<?php
+	while ($dataStages = $reqListeStageEtu->fetch()) {
+?>
+		<tr>
+			<td><?php echo $dataStages['Intitule']; ?>							</td>
+			<td><?php echo $dataStages['NomEntr']; ?>							</td>
+			<td><?php echo $dataStages['Creneau']; ?>							</td>
+			<td>
+				<form 	action="./espace_etudiant.php" 
+						method="POST">
+					<input 	type="hidden"
+							name="idEtudiant"
+							value="<?php echo $dataStages['Id_etudiant']; ?>">
+					<input 	type="hidden" 
+							name="idStage"
+							value="<?php echo $dataStages['Id_stage']; ?>">
+					<button type="submit" 
+							name="desinscrireStage">
+				 		Se désinscrire
+				 	</button>
+				</form>															</td>
+		</tr>
+<?php
+	}
+?>
+	</table>  	        
+<?php
+    }else{
+        echo "Merci de vous <a href='index.php'>connecter</a> pour accéder à cette page.";
+    }
+?>
+	</main>
+</body>
 </html>
