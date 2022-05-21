8862c8d45aeed352cbd95b20244e36b5abc9e015
lucasfaget <105640969+lucasfaget@users.noreply.github.com>
2022-05-21 18:48:41 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,5 @@
 <?php
 
-	/* ----A UTILISER SUR LE SITE d'HEBERGEMENT -------
-	$server = 'fdb28.awardspace.net';
-	$db     = '3788709_forumstage';
-	$login  = '3788709_forumstage';
-	$mdp    = '4frHtZ+S46M:WArK';
-	--------------------------------------------------*/
-
 	/* ----A UTILISER EN LOCALHOST ------------------ */
 	$server = 'localhost';
 	$login  = 'root';
